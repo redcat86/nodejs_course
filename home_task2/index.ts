3546f9d4c01ec0b212d7af16d7a4b88ce8ed9471
Natalia Popova <redcat@gmail.com>
2022-12-21 00:59:38 +0100

    Task1: added functions add, edit and get userlist

--- home_task2/index.ts
+++ home_task2/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 const app = express();
 
+app.use(express.json());
+
 const users: User[] = [
   {
     id: "number_one",
@@ -11,12 +13,14 @@ const users: User[] = [
   },
 ];
 
+app.locals["users"] = users;
+
 app.get("/", (req: Request, res: Response) => {
   res.json({ ok: true });
 });
 
 app.get("/userlist", (req: Request, res: Response) => {
-  res.json({ ok: true, users });
+  res.json({ ok: true, users: app.locals.users });
 });
 
 app.get("/user/:id", (req: Request, res: Response) => {
@@ -24,6 +28,36 @@ app.get("/user/:id", (req: Request, res: Response) => {
     ok1: true,
     user: users.find((user) => user.id === req.params.id),
   });
+});
+
+app.post("/adduser", (req: Request, res: Response) => {
+  const users = app.locals.users;
+
+  //  validation should be added for req.body
+  const duplicate = users.find((user: User) => user.id === req.body.id);
+
+  if (duplicate) {
+    res.status(500).send({ message: "User already exist." });
+  } else {
+    app.locals.users.push(req.body);
+    res.json({ ok: true, message: "User added" });
+  }
+});
+
+app.post("/edituser/:id", (req: Request, res: Response) => {
+  const users = app.locals.users;
+
+  const id = req.params.id;
+  const user: User = users.find((u: User) => u.id === id);
+
+  if (!user) {
+    res.status(500).send({ message: "User dosn't exist." });
+  } else {
+    user.age = req.body.age;
+    user.login = req.body.login;
+    user.password = req.body.password;
+    res.json({ ok: true, message: "User has been updated." });
+  }
 });
 
 const PORT = 3000;
